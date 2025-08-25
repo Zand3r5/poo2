f0ba0be037dfddc1e79059b8bdcdce16e324774f
Zand3r5 <andresluza@uca.edu.ar>
2025-08-25 20:43:50 -0300

    pagarViaje terminado

--- practica2/Sube.js
+++ practica2/Sube.js
@@ -6,8 +6,18 @@ const Sube =function (identificador, saldo = 0) {
     this.saldo= saldo;
 
     this.obtenerIdentificador =()=> this.identificador;
-    this.obtenerSaldoMinimo =()=> this.saldoMinimo;
-    this.obtenerSaldo = () => saldo;
+    this.obtenerSaldoMinimo =()=> saldoMinimo;
+    this.obtenerSaldo = () => this.saldo;
+    this.cargarSaldo = (recarga) => this.saldo += recarga;
+    this.pagarViaje = (montoAPagar) => {
+        this.verificarSaldo(montoAPagar);
+        this.saldo -= montoAPagar;
+    }
+    this.verificarSaldo = (montoAPagar) => {
+        if (this.saldo - montoAPagar < saldoMinimo ){
+            throw new Error("Saldo insuficiente");
+        };
+    }
 }
 
 module.exports = Sube;
--- practica2/Sube.test.js
+++ practica2/Sube.test.js
@@ -16,10 +16,29 @@ test("Todas las subes tienen un saldo minimo",()=>{
     expect(sube2.obtenerSaldoMinimo()).toBe(-999);
 })
 
-test("Cargamos saldo en una sube", ()=>{
+test("Verificamos el saldo inicial en una sube", ()=>{
     sube1 = new Sube(1,100);
     sube2 = new Sube(2);
 
     expect(sube1.obtenerSaldo()).toBe(100);
     expect(sube2.obtenerSaldo()).toBe(0);
+});
+
+test("Cargamos saldo en una sube", ()=>{
+    sube1 = new Sube(1,100);
+    sube2 = new Sube(2);
+
+    sube1.cargarSaldo(100);
+    sube2.cargarSaldo(100);
+    expect(sube1.obtenerSaldo()).toBe(200);
+    expect(sube2.obtenerSaldo()).toBe(100);
+});
+
+test("Podemos pagar un viaje con nuestra sube", ()=>{
+    sube1 = new Sube(1,100);
+    sube1.cargarSaldo(100);
+    sube1.pagarViaje(150);
+    expect(sube1.obtenerSaldo()).toBe(50);
+    sube1.pagarViaje(100);
+    expect(() => sube1.pagarViaje(1000)).toThrow("Saldo insuficiente");
 })
