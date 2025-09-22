e483ef473a502af6f887f8a2b0cd8a8fd32cd9d6
Zand3r5 <andresluza@uca.edu.ar>
2025-09-22 18:43:48 -0300

    remove node from repo

--- practica5/MarksRover.js
+++
@@ -1,7 +0,0 @@
-'use-strict';
-
-const MarksRover = function(){
-
-}
-
-module.exports = MarksRover;
---
+++ practica5/MarsRover.js
@@ -0,0 +1,7 @@
+'use-strict';
+
+const MarsRover = function(){
+
+}
+
+module.exports = MarsRover;
