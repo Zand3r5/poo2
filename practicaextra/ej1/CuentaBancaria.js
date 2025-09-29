ef368070e036096202431c02c68d38a665e72e6c
Zand3r5 <andresluza@uca.edu.ar>
2025-09-29 16:35:58 -0300

    Extra

---
+++ practicaextra/ej1/CuentaBancaria.js
@@ -0,0 +1,6 @@
+'use-strict'
+
+const CuentaBancaria = function (){
+    this.saldo = 0;
+}
+module.exports = CuentaBancaria;
---
+++ practicaextra/ej1/CuentaBancaria.test.js
@@ -0,0 +1,9 @@
+import CuentaBancaria from "./CuentaBancaria";
+
+'use-strict'
+
+const CuentaBancaria = require('../ej1/CuentaBancaria');
+
+test('Primeros movimientos cuenta bancaria',()=>{
+    cuentaBancaria = new CuentaBancaria();
+});
